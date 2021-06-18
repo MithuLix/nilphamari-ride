aac3ecf72bb7348bfb2d0d61dc0f9e07a98bdf12
mithulix <mohaiminul49@gmail.com>
2021-06-18 22:01:49 +0600

    nilphamari-ride

---
+++ src/Components/FirebaseManager/firebase.config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFx09RiHhbAq33H-omos-Jisk3yazeleA",
+    authDomain: "nilphamari-ride.firebaseapp.com",
+    projectId: "nilphamari-ride",
+    storageBucket: "nilphamari-ride.appspot.com",
+    messagingSenderId: "420767758146",
+    appId: "1:420767758146:web:c07e29d3e74fce0fe5fadb",
+    measurementId: "G-W6DJJG4KS2"
+  };
+
+export default firebaseConfig;
