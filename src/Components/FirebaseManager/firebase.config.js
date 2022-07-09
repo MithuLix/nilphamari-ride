10608c78c7fc3a05070de2141b829555a3f9fed0
mithulix <mohaiminul.devs@gmail.com>
2022-07-09 06:13:33 -0400

    new

--- src/Components/FirebaseManager/firebase.config.js
+++ src/Components/FirebaseManager/firebase.config.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFx09RiHhbAq33H-omos-Jisk3yazeleA",
-    authDomain: "nilphamari-ride.firebaseapp.com",
-    projectId: "nilphamari-ride",
-    storageBucket: "nilphamari-ride.appspot.com",
-    messagingSenderId: "420767758146",
-    appId: "1:420767758146:web:c07e29d3e74fce0fe5fadb",
-    measurementId: "G-W6DJJG4KS2"
+  apiKey: "AIzaSyCRGkxaadwJAX6XmB3ezrMuBaZdtaGljiU",
+  authDomain: "nilphamari-ride-c837e.firebaseapp.com",
+  projectId: "nilphamari-ride-c837e",
+  storageBucket: "nilphamari-ride-c837e.appspot.com",
+  messagingSenderId: "525526411423",
+  appId: "1:525526411423:web:d549d5a462d57a0d48daaa",
+  measurementId: "G-V55598BK7J"
   };
 
 export default firebaseConfig;
